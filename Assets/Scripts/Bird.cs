16074562455b43e7c193faf133e558bb03976a97
unknown <gunawan48160@gmail.com>
2021-02-08 23:04:01 +0700

    ExplosionDone

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Bird : MonoBehaviour
+{
+    public enum BirdState { Idle, Thrown, HitSomething }
+    public GameObject Parent;
+    public Rigidbody2D RigidBody;
+    public CircleCollider2D Collider;
+
+    public BirdState _state;
+    public BirdState State{ get { return _state; } }
+
+    private float _minVelocity = 0.05f;
+    private bool _flagDestroy = false;
+
+    public UnityAction OnBirdDestroyed = delegate { };
+    public UnityAction<Bird> OnBirdShot = delegate { };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RigidBody.bodyType = RigidbodyType2D.Kinematic;
+        Collider.enabled = false;
+        _state = BirdState.Idle;
+    }
+
+    private void FixedUpdate()
+    {
+        if(_state == BirdState.Idle && 
+            RigidBody.velocity.sqrMagnitude >= _minVelocity)
+        {
+            _state = BirdState.Thrown;
+        }
+
+        if((_state == BirdState.Thrown || _state == BirdState.HitSomething) &&
+            RigidBody.velocity.sqrMagnitude < _minVelocity &&
+            !_flagDestroy)
+        {
+            // Destroy gameObject after 2 seconds
+            // If the velocity is already below minimum
+            _flagDestroy = true;
+            StartCoroutine(DestroyAfter(2));
+        }
+    }
+
+    private IEnumerator DestroyAfter(float second)
+    {
+        yield return new WaitForSeconds(second);
+        Destroy(gameObject);
+    }
+
+    public void MoveTo(Vector2 target, GameObject parent)
+    {
+        gameObject.transform.SetParent(parent.transform);
+        gameObject.transform.position = target;
+    }
+
+    public void Shoot(Vector2 velocity, float distance, float speed)
+    {
+        Collider.enabled = true;
+        RigidBody.bodyType = RigidbodyType2D.Dynamic;
+        RigidBody.velocity = velocity * speed * distance;
+
+        OnBirdShot(this);
+    }
+
+    private void OnDestroy()
+    {
+        if(_state == BirdState.Thrown || _state == BirdState.HitSomething)
+            OnBirdDestroyed();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _state = BirdState.HitSomething;
+        OnBirdHit();
+    }
+
+    public virtual void OnTap()
+    {
+
+    }
+
+    public virtual void OnBirdHit()
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/ExplosiveBird.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveBird : Bird
+{
+    public float radius = 5.0F;
+    public float power = 10.0F;
+
+    public bool _hasExplode = false;
+
+    public SpriteRenderer radiusRenderer;
+
+
+    private void Update()
+    {
+        radiusRenderer.size = new Vector2(radius, radius);
+
+    }
+    public override void OnTap()
+    {
+        radiusRenderer.enabled = true;
+        radiusRenderer.size = new Vector2(radius, radius);
+    }
+
+    public override void OnBirdHit()
+    {
+        base.OnBirdHit();
+        Explode();
+    }
+
+    public void Explode()
+    {
+        if (!_hasExplode)
+        {
+            Vector3 explosionPos = transform.position;
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+            foreach (Collider2D hit in colliders)
+            {
+                if (hit.gameObject.name != this.gameObject.name)
+                {
+                    Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+
+                    Vector2 dir = hit.transform.position - this.transform.position;
+                    Vector2 dirNormalized = dir.normalized;
+                    float magnitude = dir.magnitude;
+
+                    if (rb != null)
+                    {
+                        print(dir);
+                        print(dirNormalized);
+                        print(magnitude);
+
+                        rb.AddForce(dir.normalized * (1 / magnitude) * power, ForceMode2D.Impulse);
+
+                        _hasExplode = true;
+                    }
+                }
+
+            }
+        }
+    }
+}
